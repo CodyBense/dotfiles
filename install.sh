6ceaabdca31c0e97845f30a7acd569efc6dab888
CodyBense <codybense@gmail.com>
2024-09-09 17:31:58 -0400

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/zsh
 
+# Install yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git ~/Downloads
+cd ~/Downloads/yay
+mkpkg -si
 
+# Install pacman packages
+sudo pacman -S arduino-cli arduino-ide bat brighnessctl cmake dolphin direnv dmenu docker fastfetch firefox fzf go hypridle hyprland hyprlock kitty lazygit lf libnotify neovim network-manager-applet nerd-fonts noto-fonts-cjk noto-fonts-emoji pavucontrol pipewire-pulse ripgrep rofi-wayland starship stow swayimg swaync swww tlp tmux tree ttf-font-awesome unzip wl-clipboard yad zoxide zsh
+
+# Install yay packages
+yay -S rustmon-git tlpui trashy ttf-material-icons-git wlogout zen-browser-bin
