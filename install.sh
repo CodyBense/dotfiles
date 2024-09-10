58b4e5e429544faec7b7b3ab762423db04da2224
CodyBense <codybense@gmail.com>
2024-09-09 20:13:54 -0400

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Install yay
 sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git ~/Downloads
-cd ~/Downloads/yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
 mkpkg -si
 
 # Install pacman packages
@@ -11,3 +11,28 @@ sudo pacmand -S arduino-cli arduino-ide bat brightnessctl cmake direnv dmenu doc
 
 # Install yay packages
 yay -S rustmon-git tlpui trashy ttf-material-icons-git wlogout zen-browser-bin
+
+# Make some directorys
+mkdir -p ~/workspaces/github.com/CodyBense
+mkdir -p ~/workspaces/Projects
+
+# Clone dotfiles repo
+git clone https://github.com/CodyBense/dotfiles.git
+
+# Stow commands for dotfiles
+cd ~/dotfiles
+stow emoji
+stow git
+stow hypr
+stow kitty
+stow lf
+stow nvim
+stow rofi
+stow scripts
+stow starship
+stow swaync
+stow tmux
+stow wallpapers
+stow waybar
+stow wlogout
+stow zsh
