9de6fd5f31cb827c36b4045ee45d147c3ca43e29
CodyBense <codybense@gmail.com>
2025-02-11 18:50:59 -0500

    added nvim config back, now not linked to CodyBense/nvim

---
+++ nvim/.config/nvim/lua/cody/plugins/treesitter.lua
@@ -0,0 +1,33 @@
+-- look through primeagen to see what i want to change
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        build = ":TSUpdate",
+        config = function()
+            local treesitter = require("nvim-treesitter.configs")
+
+            treesitter.setup({
+                highlight = {
+                    enable = true,
+                },
+
+                indent = { enable = true },
+                ensure_installed = {
+                    "json",
+                    "javascript",
+                    "typescript",
+                    "html",
+                    "css",
+                    "lua",
+                    "python",
+                    "c",
+                    "vimdoc",
+                    "vim",
+                    "rust",
+                    "go",
+                }
+            })
+        end
+    },
+}
