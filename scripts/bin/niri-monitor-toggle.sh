079276cd7a8eee2a3fa69d86a8f77479675e61c7
CodyBense <codybense@gmail.com>
2025-09-10 17:11:16 -0400

    niri-monitor-toggle: fixed for when I turn on my laptop

--- scripts/bin/niri-monitor-toggle.sh
+++ scripts/bin/niri-monitor-toggle.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 
-monitors=$(niri msg --json outputs | jq '[.[] | select( .current_mode == 0)]' | jq '[.[].name] | join(" ")' -r)
-read -ra monitors_arr <<< "${monitors}"
-focused_window=$(niri msg --json workspaces | jq '.[] | select (.is_focused == true)' | jq '.active_window_id' -r)
+monitors=
+monitors_arr=()
 monitor_1="eDP-1"
 monitor_2="DP-3"
+focused_window=$(niri msg --json workspaces | jq '.[] | select (.is_focused == true)' | jq '.active_window_id' -r)
+
+get_monitors () {
+    monitors=$(niri msg --json outputs | jq '[.[] | select( .current_mode == 0)]' | jq '[.[].name] | join(" ")' -r)
+    read -ra monitors_arr <<< "${monitors}"
+}
+
+get_monitors
 
 if [ -z ${monitors_arr[1]} ]; then
-    echo "one monitor"
     if [ ${monitors_arr[0]} == ${monitor_2} ]; then
         # external on and internal off
         niri msg output ${monitor_1} on
@@ -17,8 +23,8 @@ if [ -z ${monitors_arr[1]} ]; then
         niri msg action focus-window --id ${focused_window}
         notify-send "Sending workspaces to internal monitor"
     elif [ ${monitors_arr[0]} == ${monitor_1} ]; then
-        niri msg output ${monitor_2} on
-        if [ $? == 0 ]; then
+        get_monitors
+        if [ ${monitors_arr[0]} != ${monitor_1} ]; then
             sleep 0.5
             niri msg output ${monitor_1} off
             niri msg action focus-window --id ${focused_window}
