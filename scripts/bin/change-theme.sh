fd5760f1afa41f432c72732d932bf735f52d43bc
CodyBense <codybense@gmail.com>
2025-09-11 17:13:28 -0400

    scripts: created a rofi based theme changer script

--- scripts/bin/change-theme.sh
+++ scripts/bin/change-theme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-change () { \
+change () { 
     ln -sf ~/.local/share/colors/$1/$1.css ~/.config/waybar/colors.css
     ln -sf ~/.local/share/colors/$1/$1.rasi ~/.config/rofi/colors.rasi
     # ln -sf ~/dotfiles/bemenu/.config/bemenu/bemenu-$1 ~/dotfiles/scripts/bin/bemenu-launch
@@ -8,7 +8,7 @@ change () { \
     bash restart-waybar.sh
 }
 
-selected () { \
+selected () { 
     choice=$(echo -e "catppuccin-mocha\ngruvbox" | bemenu -c -l 5 -i -p "Change theme: ")
     case $choice in 
         "catppuccin-mocha") change "catppuccin-mocha" ;;
