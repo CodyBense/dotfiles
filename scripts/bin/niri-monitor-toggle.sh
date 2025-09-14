21262200cf16ad3fc469cd1d746f7c6276da09aa
CodyBense <codybense@gmail.com>
2025-09-14 19:19:14 -0400

    scripts, niri-monitor-toggle: reverted to prior version

--- scripts/bin/niri-monitor-toggle.sh
+++ scripts/bin/niri-monitor-toggle.sh
@@ -15,30 +15,24 @@ get_monitors () {
 get_monitors
 
 if [ -z ${monitors_arr[1]} ]; then
-    if [ ${monitors_arr[0]} == ${monitor_2} ]; then
+    if [ "${monitors_arr[0]}" == "${monitor_2}" ]; then
         # external on and internal off
         niri msg output ${monitor_1} on
         sleep 0.5
         niri msg output ${monitor_2} off
         niri msg action focus-window --id ${focused_window}
         notify-send "Sending workspaces to internal monitor"
-    elif [ ${monitors_arr[0]} == ${monitor_1} ]; then
-        niri msg output ${monitor_2} on
-        sleep 0.5
-        niri msg output ${monitor_1} off
-        niri msg action focus-window --id ${focused_window}
-        notify-send "Sending workspaces to external monitor"
-        # get_monitors
-        # if [ ${monitors_arr[0]} != ${monitor_1} ]; then
-        #     sleep 0.5
-        #     niri msg output ${monitor_1} off
-        #     niri msg action focus-window --id ${focused_window}
-        #     notify-send "Sending workspaces to external monitor"
-        # fi
+    elif [ "${monitors_arr[0]}" == "${monitor_1}" ]; then
+        get_monitors
+        if [ "${monitors_arr[0]}" != "${monitor_1}" ]; then
+            sleep 0.5
+            niri msg output ${monitor_1} off
+            niri msg action focus-window --id ${focused_window}
+            notify-send "Sending workspaces to external monitor"
+        fi
 
     fi
 else
-    sleep 1
     niri msg output ${monitor_1} off
     niri msg action focus-window --id ${focused_window}
     notify-send "Sending workspaces to external monitor"
