4c5d9ddc24ee30f7d4917b3630794793737c688f
CodyBense <codybense@gmail.com>
2025-09-24 19:34:03 -0400

    nvim: changed vim.o to vim.opt and set local remap for vim.keymap.set to map

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,46 +1,55 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.wrap = false
-vim.o.hlsearch = false
-vim.o.incsearch = true
-vim.o.termguicolors = true
-vim.o.scrolloff = 8
-vim.o.signcolumn = "yes"
--- vim.o.isfname:append("@-@")
-vim.o.updatetime = 50
-vim.o.colorcolumn = "80"
-vim.o.swapfile = false
-vim.o.winborder = "rounded"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+vim.opt.swapfile = false
+vim.opt.winborder = "rounded"
 vim.g.mapleader = " "
 
--- allows moving of selected lines and autoindent
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- add next line at end and keeps cursor in current position
-vim.keymap.set("n", "J", "mzJ`z")
--- half page jumps and keeys cursor in middle
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- keeps cursor in place when searching terms
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
--- copys over and puts replace in void register
-vim.keymap.set("x", "<leader>p", "\"_dP")
--- copys to computer clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
--- deletes to void register
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
--- Q is a bad place?
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+-- Keymaps
+local map = vim.keymap.set
 
+-- allows moving of selected lines and autoindent
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+-- add next line at end and keeps cursor in current position
+map("n", "J", "mzJ`z")
+-- half page jumps and keeys cursor in middle
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+-- keeps cursor in place when searching terms
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+-- copys over and puts replace in void register
+map("x", "<leader>p", "\"_dP")
+-- copys to computer clipboard
+map({ "n", "v" }, "<leader>y", "\"+y")
+map("n", "<leader>Y", "\"+Y")
+-- deletes to void register
+map("n", "<leader>d", "\"_d")
+map("v", "<leader>d", "\"_d")
+-- Q is a bad place?
+map("n", "Q", "<nop>")
+map("n", "<leader><leader>x", "<cmd>source %<CR>")
+-- lsp format
+map("n", "<leader>lf", vim.lsp.buf.format)
+-- Oil
+map("n", "<leader>pv", "<CMD>Oil<CR>")
+-- Mini.pick
+map("n", "<leader>f", ":Pick files<CR>")
+map("n", "<leader>gf", ":Pick files tool='git'<CR>")
+map("n", "<leader>h", ":Pick help<CR>")
 
 vim.pack.add({
     { src = 'https://github.com/catppuccin/nvim' },
@@ -50,8 +59,26 @@ vim.pack.add({
     { src = 'https://github.com/mason-org/mason.nvim' },
 })
 
+vim.cmd("colorscheme catppuccin-mocha")
+
+require "oil".setup()
+
+require "mini.pick".setup()
+
 require "mason".setup()
-vim.lsp.enable({ "lua_ls", "jedi_language_server" })
+vim.lsp.enable(
+    {
+        "lua_ls",
+        "jedi_language_server",
+        "bash_language_server",
+        "beautysh",
+        "cbfmt",
+        "gopls",
+        "rust_analyzer",
+        "yaml_language_server",
+
+    }
+)
 
 vim.lsp.config("lua_ls", {
     settings = {
@@ -63,7 +90,6 @@ vim.lsp.config("lua_ls", {
     }
 })
 
-vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('my.lsp', {}),
     callback = function(args)
@@ -77,11 +103,4 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
-require "oil".setup()
-vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
-
-require "mini.pick".setup()
-vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
-vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
-
-vim.cmd("colorscheme catppuccin-mocha")
+vim.cmd [[set completeopt+=menuone,noselect,popup]]
