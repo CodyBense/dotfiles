01c1a26d4384300f44a897058cbce90d5b8bd92a
CodyBense <codybense@gmail.com>
2025-09-26 13:03:09 -0400

    notes.sh: reverted back to hostname

--- scripts/bin/notes.sh
+++ scripts/bin/notes.sh
@@ -3,7 +3,7 @@
 choice="$(printf "pl\nph" | tofi)" || exit 0
 
 case $choice in
-    pl) rsync -rtu cody@192.168.1.243:/data/Obsidian $HOME/Documents && notify-send "Pulling notes is done";;
-    ph) rsync -rtu $HOME/Documents cody@192.168.1.243:/data/Obsidian && notify-send "Pushing notes is done";;
+    pl) rsync -rtu cody@vault:/data/Obsidian $HOME/Documents && notify-send "Pulling notes is done";;
+    ph) rsync -rtu $HOME/Documents cody@vault:/data/Obsidian && notify-send "Pushing notes is done";;
 esac
 
