31a35cea7e6b24a7fa7bce8fd7cbe993a8fa1788
CodyBense <codybense@gmail.com>
2025-10-01 19:01:59 -0400

    scripts, notes: switched to rofi from tofi

--- scripts/bin/notes.sh
+++ scripts/bin/notes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-choice="$(printf "pl\nph" | tofi.sh)" || exit 0
+choice="$(printf "pl\nph" | rofi -dmenu -p "Notes: ")" || exit 0
 
 case $choice in
     pl) rsync -rtu cody@vault:/data/Obsidian $HOME/Documents && notify-send "Pulling notes is done";;
