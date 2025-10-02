bc85c015366326fcf37383c2cebf60fd1e9d845b
CodyBense <codybense@gmail.com>
2025-10-02 16:45:36 -0400

    scripts, project-launch: updated to change class name as well

--- scripts/bin/project-launch.sh
+++ scripts/bin/project-launch.sh
@@ -7,16 +7,16 @@ options=$(printf "${other_projects}\n$(ls $DIR)\n" | rofi -dmenu -p "Projects: "
 
 case "${options}" in
     "dotfiles")
-        ghostty --title="project" --working-directory=$HOME/dotfiles
+        ghostty --class="com.project.ghostty" --title="$options" --working-directory=$HOME/dotfiles
         ;;
     "new")
         project_name="$(printf '' | rofi -dmenu -p 'Project Name: ')"
         [ -n "$project_name" ] || exit 0
         echo $DIR/$project_name
         mkdir -p $DIR/$project_name
-        ghostty --title="project" --working-directory=$DIR/$project_name
+        ghostty --class="com.project.ghostty" --title="$options" --working-directory=$DIR/$project_name
         ;;
     *)
-        ghostty --title="project" --working-directory=$DIR/$options
+        ghostty --class="com.project.ghostty" --title="$options" --working-directory=$DIR/$options
         ;;
 esac
