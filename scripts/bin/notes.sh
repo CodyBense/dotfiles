6fa17f7cd7230f72208c0e0cc03da89a4351cb10
CodyBense <codybense@gmail.com>
2025-10-01 21:01:29 -0400

    scripts, notes: fixed path to send over ssh

--- scripts/bin/notes.sh
+++ scripts/bin/notes.sh
@@ -4,6 +4,6 @@ choice="$(printf "pl\nph" | rofi -dmenu -p "Notes: ")" || exit 0
 
 case $choice in
     pl) rsync -rtu cody@vault:/data/Obsidian $HOME/Documents && notify-send "Pulling notes is done";;
-    ph) rsync -rtu $HOME/Documents cody@vault:/data/Obsidian && notify-send "Pushing notes is done";;
+    ph) rsync -rtu $HOME/Documents/Obsidian/* cody@vault:/data/Obsidian && notify-send "Pushing notes is done";;
 esac
 
