c6a2f375489bdd9dd50cb068b9ba73de3b480281
CodyBense <codybense@gmail.com>
2025-10-04 16:59:46 -0400

    scripts: addign more feats to notes-launcher
    can create new daily note, will add more later
    open ceratin note dirs
    updated sync command

--- scripts/bin/notes-launcher.sh
+++ scripts/bin/notes-launcher.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash
 
-folder=/home/codybense/Documents/Obsidian/
+DIR=$HOME/Documents/Obsidian/
 
-sync () { \
-    rsync -rtu $HOME/Documents/Obsidian cody@vault:/data && rsync -rtu cody@vault:/data/Obsidian $HOME/Documents && notify-send "Syncing notes is complete"
-}
-
-selected () { \
-    choice=$(echo "Sync\n$(command ls -t1 $folder)" | bemenu-launch -c -l 5 -i -p "Notes menu: ")
-    case $choice in 
-        "Sync") sync ;;
+new_notes() {
+    new_options=$(printf "daily\nnotes\nprojects" | rofi -dmenu -p "Notes: ") || exit 0
+    case "$new_options" in
+        "daily")
+            nvim $DIR/daily_notes/$(command date +"%Y-%m-%d").md
+            ;;
+        *)
+            notify-send "Figure out how you want to implement new none daily notes"
+            echo "- [ ] Figure out how you want to implement new none daily notes" >> $DIR/daily_notes/TODO.md
+            ;;
     esac
 }
-
-selected
+choice=$(printf "sync\nopen\nnew" | rofi -dmenu -p "Notes menu: ") || exit 0
+case $choice in 
+    "sync") 
+        rsync -rtu $HOME/Documents/Obsidian/* cody@vault:/data/Obsidian && rsync -rtu cody@vault:/data/Obsidian $HOME/Documents && notify-send "Syncing notes is complete"
+        ;;
+    "open")
+        note_dir=$(printf "$(command ls -t1 $DIR)" | rofi -dmenu -p "Note dir: ") || exit 0
+        echo $note_dir
+        ghostty --class="com.notes.ghostty" --title="notes" --working-directory=$DIR/$note_dir
+        ;;
+    "new")
+        new_notes
+        ;;
+    "*")
+        exit 1
+        ;;
+esac
