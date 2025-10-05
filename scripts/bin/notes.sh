23b1009e9a0707d3b356d0a11c0a0ccf7d85879b
CodyBense <codybense@gmail.com>
2025-10-05 16:27:43 -0400

    REMOVED: unneeded scripts

--- scripts/bin/notes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-choice="$(printf "pl\nph" | rofi -dmenu -p "Notes: ")" || exit 0
-
-case $choice in
-    pl) rsync -rtu cody@vault:/data/Obsidian $HOME/Documents && notify-send "Pulling notes is done";;
-    ph) rsync -rtu $HOME/Documents/Obsidian/* cody@vault:/data/Obsidian && notify-send "Pushing notes is done";;
-esac
-
--- scripts/bin/rofi-notes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-choice="$(printf "pl\nph" | rofi -dmenu)" || exit 0
-
-case $choice in
-    pl) rsync -rtu cody@192.168.1.243:/data/Obsidian $HOME/Documents && notify-send "Pulling notes is done";;
-    ph) rsync -rtu $HOME/Documents/Obsidian cody@192.168.1.243:/data && notify-send "Pushing notes is done";;
-esac
-
