21379c0478dab85b927db72afc5d47da3e0eb467
CodyBense <codybense@gmail.com>
2025-10-10 08:46:25 -0400

    scripts, notes-launcher: added launching obsidian into selected vault/dir and refocus nvim

--- scripts/bin/notes-launcher.sh
+++ scripts/bin/notes-launcher.sh
@@ -22,7 +22,11 @@ case $choice in
     "open")
         note_dir=$(printf "$(command ls -t1 $DIR)" | rofi -dmenu -p "Note dir: ") || exit 0
         echo $note_dir
-        ghostty --class="com.notes.ghostty" --title="notes" --working-directory=$DIR/$note_dir
+        ghostty --class="com.notes.ghostty" --title="notes" --working-directory=$DIR/$note_dir &
+        obsidian obsidian://open?vault="$note_dir" &
+        sleep 2
+        niri msg action focus-workspace notes
+        niri msg action focus-column 0
         ;;
     "new")
         new_notes
