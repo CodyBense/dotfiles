ed50a436d52b155cf3f1d14dfcaf86eac96b815b
CodyBense <codybense@gmail.com>
2025-11-07 07:41:36 -0500

    ADDED/UPDATE: create new projects launch script and bound it in niri
    New script uses zellij

---
+++ scripts/bin/project-launch-zellij.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -eu
+
+DIR=$HOME/workspaces/github/CodyBense
+other_projects="new\ndotfiles"
+options=$(printf "${other_projects}\n$(ls $DIR)\n" | rofi -dmenu -p "Projects: ")
+
+case "${options}" in
+    "dotfiles")
+        ghostty --class="com.project.ghostty" --title="$options" --working-directory=$HOME/dotfiles -e zellij a -c dotfiles
+        ;;
+    "new")
+        project_name="$(printf '' | rofi -dmenu -p 'Project Name: ')"
+        [ -n "$project_name" ] || exit 0
+        echo $DIR/$project_name
+        mkdir -p $DIR/$project_name
+        ghostty --class="com.project.ghostty" --title="$options" --working-directory=$DIR/$project_name -e zellij a -c $project_name
+        ;;
+    *)
+        ghostty --class="com.project.ghostty" --title="$options" --working-directory=$DIR/$options -e zellij a -c $options
+        ;;
+esac
