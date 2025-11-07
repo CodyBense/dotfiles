e862f93e2ef10952764c99dd68fa2d6939de17d4
CodyBense <codybense@gmail.com>
2025-11-06 21:18:41 -0500

    UPDATE: changed to read packages from files

--- install-arch.sh
+++ install-arch.sh
@@ -15,146 +15,15 @@ echo "----------------------------------"
 echo ""
 sleep 1
 
-packages=(
-    7zip
-    awesome-terminal-fonts
-    base
-    base-devel
-    bat
-    bemenu-wayland
-    bluetui
-    bluez
-    bluez-utils
-    brightnessctl
-    btop
-    btrfs-progs
-    cargo
-    chromium
-    cifs-utils
-    cmake
-    cronie
-    cyme
-    direnv
-    docker-compose
-    dolphin
-    dosfstools
-    efibootmgr
-    esptool
-    fastfetch
-    firefox
-    flatpak
-    freecad
-    fzf
-    ghostty
-    gimp
-    git
-    github-cli
-    github-desktop
-    go
-    gst-plugin-pipewire
-    hypridle
-    hyprland
-    hyprlock
-    hyprpaper
-    hyprpicker
-    imv
-    intel-ucode
-    jq
-    jqp-bin
-    kanata
-    kicad
-    kicad-library
-    kicad-library-3d
-    killall
-    kio-admin
-    kitty
-    lazygit
-    libpulse
-    linux
-    linux-firmware
-    localsend
-    man-db
-    man-pages
-    neovim-nightly-bin
-    network-manager-applet
-    networkmanager
-    niri
-    nodejs-material-design-icons
-    noto-fonts-cjk
-    noto-fonts-emoji
-    obsidian
-    openssh
-    orca-slicer-git
-    paru
-    paru-debug
-    pavucontrol
-    pipewire
-    pipewire-alsa
-    pipewire-jack
-    pipewire-pulse
-    polkit-kde-agent
-    power-profiles-daemon
-    resvg
-    rofi
-    rofi-emoji
-    rust-analyzer
-    socat
-    sof-firmware
-    spotify
-    spotify-player
-    starship
-    stow
-    stress
-    swayimg
-    swaync
-    swww
-    timeshift
-    timeshift-systemd-timer
-    tmux
-    tofi
-    trash-cli
-    tree
-    ttf-cascadia-code-nerd
-    ttf-jetbrains-mono-nerd
-    ttf-terminus-nerd
-    udiskie
-    unzip
-    usbutils
-    uv
-    uwsm
-    viber
-    vlc
-    vlc-plugin-ffmpeg
-    waybar
-    waybar-module-pacman-updates-git
-    wf-recorder
-    wget
-    wireplumber
-    wl-clipboard
-    wlogout
-    xdg-desktop-portal-gnome
-    xdg-desktop-portal-kde
-    xwayland-satellite
-    yad
-    yazi
-    zathura
-    zathura-pdf-mupdf
-    zellij
-    zen-browser-bin
-    zoxide
-    zram-generator
-)
-
-for package in "${packages[@]}"
-do
+while read -r line; do
     echo ""
     echo "----------------------------------"
     echo "-----Installing ${package} package------"
     echo "----------------------------------"
     echo ""
     sleep 1
-    sudo pacman -S ${package} --noconfirm
-done
+    sudo pacman -S "$line" --noconfirm
+done < install/packages-arch.txt
 
 echo ""
 echo "----------------------------------"
@@ -176,29 +45,15 @@ echo "----------------------------------"
 echo ""
 sleep 1
 
-aurpackages=(
-    arduino-ide
-    kanata
-    localsend
-    neovim-nightly-bin
-    orca-slicer-git
-    spotify
-    timeshift-systemd-timer
-    waybar-module-pacman-updates-git
-    wlogout
-    zen-browser-bin
-)
-
-for aurpackage in "${aurpackages[@]}"
-do
+while read -r line; do
     echo ""
     echo "----------------------------------"
     echo "-----Installing ${aurpackage} package------"
     echo "----------------------------------"
     echo ""
     sleep 1
-    paru -S ${aurpackage}
-done
+    paru -S "$line"
+done < install/packages-aur.txt
 
 echo ""
 echo "----------------------------------"
