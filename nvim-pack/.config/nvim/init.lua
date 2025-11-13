912c8689eb87e0c91d9b8f324a5aa997fa286a71
CodyBense <codybense@gmail.com>
2025-11-12 22:05:53 -0500

    UPDATE: changed obsidian.nvim picker to telescope

--- nvim-pack/.config/nvim/init.lua
+++ nvim-pack/.config/nvim/init.lua
@@ -160,7 +160,7 @@ require("obsidian").setup({
     },
 
     picker = {
-        name = "mini.pick",
+        name = "telescope.nvim",
         note_mappings = {
             new = "<C-x>",
             insert_link = "<C-l>",
